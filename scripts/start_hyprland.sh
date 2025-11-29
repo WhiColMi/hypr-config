1d639b98f32b89cc10c3e935e9ac09e3aa979b33
WhiColMi <mr.whicolmi@gmail.com>
2025-11-29 18:23:39 +0100

    trying to figure out dri prime and hybrid gpu setup

--- scripts/start_hyprland.sh
+++ scripts/start_hyprland.sh
@@ -3,14 +3,12 @@
 LOG="$HOME/.config/hypr/scripts/log"
 echo "$(date +"%Y-%m-%d_%H-%M-%S")" > $LOG
 
-./gen_configs.sh $LOG
+./gen_themes.sh $LOG
 
 echo "starting Hyprland..." >> $LOG
-if [ -e /run/bootmode/marker/powersaver ]; then
-  runner="intel-run"
-else
-  runner="nvidia-run"
-fi
-
-exec env WLR_DRM_NO_ATOMIC=1 "$runner" Hyprland
-
+# if [ -e /run/bootmode/marker/powersaver ]; then
+#   exec env WLR_DRM_NO_ATOMIC=1 intel-run Hyprland
+# else
+#   exec env WLR_DRM_NO_ATOMIC=1 nvidia-run Hyprland
+# fi
+exec Hyprland
